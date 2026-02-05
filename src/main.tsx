078c768e9ef534eb30a3154cd144f07427452e84
ADZAHO-Selom-Ami <selom-ami.adzaho@etu.univ-cotedazur.fr>
2026-02-05 15:46:17 +0100

    feat : Changement du composant ArtefactItem & Ajout des données mock #14

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,13 @@ import { createRoot } from "react-dom/client";
 import "@/index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "@/App.tsx";
-import HomePage from "@/pages/HomePage";
 
 // biome-ignore lint/style/noNonNullAssertion: TODO
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<App />}>
-					<Route index element={<HomePage />} />
-				</Route>
+				<Route index element={<App />} />
 			</Routes>
 		</BrowserRouter>
 	</StrictMode>,
