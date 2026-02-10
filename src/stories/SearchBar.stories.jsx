be298bbd5752d47b1671d756990ffcdfa97785d2
julienvassard <julien.vassard@etu.univ-cotedazur.fr>
2026-02-10 09:38:08 +0100

    #8 add componants SearchBar & Select for Pattern axe

---
+++ src/stories/SearchBar.stories.jsx
@@ -0,0 +1,24 @@
+import SearchBar from '../components/SearchBar';
+
+export default {
+  title: 'Patterns/SearchBar',
+  component: SearchBar,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export const Empty = {
+  args: {
+    value: '',
+    placeholder: 'Tapez quelque chose...',
+  },
+};
+
+export const Filled = {
+  args: {
+    value: 'Tailwind CSS',
+    placeholder: 'Rechercher...',
+  },
+};
---
+++ src/stories/Select.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Select, { Option } from '../components/Select';
+
+const meta: Meta<typeof Select> = {
+  title: 'Patterns/Select',
+  component: Select,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof Select>;
+
+const mockOptionsType: Option[] = [
+  { label: 'Préparation', value: 'preparation' },
+  { label: 'Exploration', value: 'explore' },
+  { label: 'Modélisation', value: 'mondel' },
+  { label: 'Normalisation', value: 'norm' },
+];
+
+const mockOptionsAlgos: Option[] = [
+    { label: 'K-means', value: 'kmeans' },
+    { label: 'DBSCAN', value: 'dbscan' },
+    { label: 'PCA', value: 'pca' },
+    { label: 'Random Forest', value: 'rf' },
+];
+
+export const Default: Story = {
+  args: {
+    options: mockOptionsType,
+    placeholder: 'Sélectionne un type de pattern',
+  },
+};
+
+export const WithLabel: Story = {
+  args: {
+    options: mockOptionsAlgos,
+    label: 'Les algos',
+    placeholder: 'Faites votre choix',
+  },
+};
